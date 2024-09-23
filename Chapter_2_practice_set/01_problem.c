0c8bbf79e440390ffd30365b367cdc524a03d4ef
Nexon_2006 <130163874+Adi8580@users.noreply.github.com>
2024-09-23 18:02:36 +0530

    Add files via upload

---
+++ Chapter_2_practice_set/01_problem.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int a=10;
+    int b=20;
+    printf("The value is %d",a%b);
+    
+    return 0;
+}
---
+++ Chapter_2_practice_set/tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    int a= 8457;
+    printf("The value is : 43%a is %d",43%a);
+    return 0;
+}
