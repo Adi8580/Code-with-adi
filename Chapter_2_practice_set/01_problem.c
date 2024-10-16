204072b013eeea6505ba581726b13cdb0d2b7732
Nexon_2006 <130163874+Adi8580@users.noreply.github.com>
2024-10-16 23:34:40 +0530

    Delete Chapter_2_practice_set directory

--- Chapter_2_practice_set/01_problem.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int a=10;
-    int b=20;
-    printf("The value is %d",a%b);
-    
-    return 0;
-}
--- Chapter_2_practice_set/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int a= 8457;
-    printf("The value is : 43%a is %d",43%a);
-    return 0;
-}
