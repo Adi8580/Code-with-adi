732ad3b8d70a9a4a54eb83d5f82b0ca43e299685
Nexon_2006 <130163874+Adi8580@users.noreply.github.com>
2024-10-16 23:34:25 +0530

    Delete Chapter_1 directory

--- Chapter_1/02radiusofcircle.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int r=9;
-    printf("The radius of the circle %d is %f", r ,3.14*r*r);
-    return 0;
-}
-// This is code is use for print the area of circle with radius
