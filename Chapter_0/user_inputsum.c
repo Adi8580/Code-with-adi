623375b62531f6543a54da7af2db5ac04ac831c1
Nexon_2006 <130163874+Adi8580@users.noreply.github.com>
2024-10-16 23:34:12 +0530

    Delete Chapter_0 directory

--- Chapter_0/user_inputsum.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int d1,d2,area;
-    printf("Enter Diagonal 1 and Diagonal 2 :");
-    scanf("%d %d",&d1,&d2);
-    area=(d1*d2)/2;
-    printf("Area of Rhombus is %d",area);
-}
